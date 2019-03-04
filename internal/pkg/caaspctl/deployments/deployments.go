1243f5319e6eecb4505f46e8abdf76b12955ab6b
Rafael Fernández López <ereslibre@ereslibre.es>
2019-03-04 18:44:53 +0100

    Minimal support for detecting the os release

--- internal/pkg/caaspctl/deployments/deployments.go
+++ internal/pkg/caaspctl/deployments/deployments.go
@@ -1,7 +1,10 @@
 package deployments
 
 import (
+	"bufio"
 	"io/ioutil"
+	"regexp"
+	"strings"
 )
 
 type Actionable interface {
@@ -17,6 +20,23 @@ type Target struct {
 
 func (t *Target) Target() string {
 	return t.Node
+}
+
+func (t *Target) OSRelease() (map[string]string, error) {
+	result := map[string]string{}
+	contents, err := t.DownloadFileContents("/etc/os-release")
+	if err != nil {
+		return result, err
+	}
+	scanner := bufio.NewScanner(strings.NewReader(contents))
+	matcher := regexp.MustCompile(`([^=]+)="?([^"]*)`)
+	for scanner.Scan() {
+		matches := matcher.FindAllStringSubmatch(scanner.Text(), -1)
+		for _, match := range matches {
+			result[match[1]] = match[2]
+		}
+	}
+	return result, nil
 }
 
 func (t *Target) Apply(data interface{}, states ...string) error {
--- internal/pkg/caaspctl/deployments/ssh/kubelet.go
+++ internal/pkg/caaspctl/deployments/ssh/kubelet.go
@@ -12,9 +12,15 @@ func init() {
 func kubeletConfigure() Runner {
 	runner := struct{ State }{}
 	runner.DoRun = func(t *Target, data interface{}) error {
-		t.UploadFileContents("/lib/systemd/system/kubelet.service", assets.KubeletService)
-		t.UploadFileContents("/etc/systemd/system/kubelet.service.d/10-kubeadm.conf", assets.KubeadmService)
-		t.UploadFileContents("/etc/sysconfig/kubelet", assets.KubeletSysconfig)
+		osRelease, _ := t.Target.OSRelease()
+		if osRelease["ID_LIKE"] == "debian" {
+			t.UploadFileContents("/lib/systemd/system/kubelet.service", assets.KubeletService)
+			t.UploadFileContents("/etc/systemd/system/kubelet.service.d/10-kubeadm.conf", assets.KubeadmService)
+		} else {
+			t.UploadFileContents("/usr/lib/systemd/system/kubelet.service", assets.KubeletService)
+			t.UploadFileContents("/usr/lib/systemd/system/kubelet.service.d/10-kubeadm.conf", assets.KubeadmService)
+			t.UploadFileContents("/etc/sysconfig/kubelet", assets.KubeletSysconfig)
+		}
 		t.ssh("systemctl", "daemon-reload")
 		return nil
 	}
