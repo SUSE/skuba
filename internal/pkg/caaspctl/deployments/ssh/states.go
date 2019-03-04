96eda31d719d3f1f99b2f017c9e289defee9d415
Rafael Fernández López <rfernandezlopez@suse.com>
2019-03-04 20:49:23 +0100

    Improve output

--- internal/pkg/caaspctl/deployments/ssh/states.go
+++ internal/pkg/caaspctl/deployments/ssh/states.go
@@ -12,12 +12,12 @@ type Runner func(t *Target, data interface{}) (error)
 
 func (t *Target) Apply(data interface{}, states ...string) error {
 	for _, stateName := range states {
-		log.Printf("=== %s: about to apply state %s ===\n", t.Node(), stateName)
+		log.Printf("=== about to apply state %s ===\n", stateName)
 		if state, stateExists := stateMap[stateName]; stateExists {
 			if err := state(t, data); err != nil {
-				log.Printf("=== %s: failed to apply state %s: %v ===\n", t.Node(), stateName, err)
+				log.Printf("=== failed to apply state %s: %v ===\n", stateName, err)
 			} else {
-				log.Printf("=== %s: state %s applied successfully ===\n", t.Node(), stateName)
+				log.Printf("=== state %s applied successfully ===\n", stateName)
 			}
 		} else {
 			log.Fatalf("state does not exist: %s", stateName)
