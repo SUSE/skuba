60942e683e0c1e330fb606179d8024d106d2a6a9
dmaiocchi <dmaiocchi@suse.com>
2019-03-28 09:43:10 +0100

    add new build for leap15 and issue referenced

--- ci/infra/libvirt/variables.tf
+++ ci/infra/libvirt/variables.tf
@@ -16,9 +16,10 @@ variable "pool" {
 # Cluster variables #
 #####################
 
+## fixme: see issue https://github.com/SUSE/avant-garde/issues/91
 variable "img_source_url" {
   type        = "string"
-  default     = "https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.0/images/openSUSE-Leap-15.0-OpenStack.x86_64-0.0.4-Buildlp150.12.127.qcow2"
+  default     = "https://download.opensuse.org/repositories/Cloud:/Images:/Leap_15.0/images/openSUSE-Leap-15.0-OpenStack.x86_64-0.0.4-Buildlp150.12.136.qcow2"
 }
 
 variable "repo_baseurl" {
