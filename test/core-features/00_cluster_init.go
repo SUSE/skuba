37958fb4c48f256538c81ecf1da3eecd367c4b5f
dmaiocchi <dmaiocchi@suse.com>
2019-05-16 18:46:21 +0200

    increase default timeout which were to low
    Use this for moment decrease later

--- test/core-features/00_cluster_init.go
+++ test/core-features/00_cluster_init.go
@@ -6,8 +6,8 @@ import (
 	"github.com/onsi/gomega/gexec"
         "github.com/onsi/gomega/gbytes"
 	"os"
-        "fmt"
-	"os/exec"
+        "time"
+    	"os/exec"
 )
 
 var _ = Describe("Create Caaspctl Cluster", func() {
@@ -31,13 +31,18 @@ var _ = Describe("Create Caaspctl Cluster", func() {
         // binary variables[init] configuration files written to
 	caaspctlPath := os.Getenv("GOPATH")        // devel mode . For release this can be changed.
  	caaspctl := caaspctlPath + "/bin/caaspctl" // this is the binary ( it can come from devel/release/rpm sources since path isn't hardcoded)
-      
+        
+        // wait  30 minutes max as timeout for completing command
+      	SetDefaultEventuallyTimeout(1800 * time.Second)
+        SetDefaultEventuallyPollingInterval(5 * time.Second)
+        SetDefaultConsistentlyDuration(1800 * time.Second)
+        SetDefaultConsistentlyPollingInterval(5 * time.Second)
+
 	It("00: Initialize cluster", func() {
                 By("create configuration files")
 		command := exec.Command(caaspctl, "cluster", "init", "--control-plane", controlPlaneIP, clusterName)
 		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-                fmt.Println(session.Out)
-		Eventually(session.Out).Should(gbytes.Say(".*configuration files written to"))
+                Eventually(session.Out).Should(gbytes.Say(".*configuration files written to"))
                 Expect(session).Should(gexec.Exit(), "configuration was not created")
                 Expect(err).To(BeNil(), "configuration was not created")
                 // change to created caaspctl directory
@@ -49,8 +54,9 @@ var _ = Describe("Create Caaspctl Cluster", func() {
                 By("add master00 to the cluster")
          	command = exec.Command(caaspctl, "node", "bootstrap", "--user", username, "--sudo", "--target", master00IP, master00Name)
 		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+                
                 // cmd should be execute without error
-                Eventually(session.Out).Should(gbytes.Say(".*state kubeadm.init applied successfully"))
+                Expect(session.Wait().Out.Contents()).Should(ContainSubstring("kubeadm.init applied successfully"))
                 Expect(session).Should(gexec.Exit(), "caaspctl adding master00 failed:")
                 Expect(err).To(BeNil(), "caaspctl adding master00 failed:")	
 	
