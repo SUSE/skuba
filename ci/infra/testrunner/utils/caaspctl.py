0affa648f23cd2f6ceca82803c7f3eb6a8aede07
Pablo Chacin <pchacin@suse.com>
2019-05-17 18:40:30 +0200

    Refactor logic for cluster setup and modification
    Move logic for cluster setup and topology modification
    (add/remove) nodes to a separate class, leaving in
    Caaspctl the logic for calling individual caaspctl
    functions.
    
    Also, remove the prefix `caaspctl` prefix from
    functions in Caaspctl class, as result redundant.
    
    Signed-off-by: Pablo Chacin <pchacin@suse.com>

--- ci/infra/testrunner/utils/caaspctl.py
+++ ci/infra/testrunner/utils/caaspctl.py
@@ -10,7 +10,6 @@ class Caaspctl:
         self.conf = conf
         self.utils = Utils(self.conf)
         self.state = self._load_tfstate()
-        self._num_master, self._num_worker = self._load_num_of_nodes()
 
     @timeout(600)
     @step
@@ -26,19 +25,6 @@ class Caaspctl:
         print("Building caaspctl")
         self.utils.gorun("make")
 
-    @timeout(600)
-    @step
-    def bootstrap_environment(self):
-        """Bootstrap Environment"""
-        self.utils.setup_ssh()
-        self.caaspctl_cluster_init()
-        self.caaspctl_node_bootstrap()
-        self.add_worker_in_cluster()
-        try:
-            self.caaspctl_cluster_status()
-        except:
-            pass
- 
     @step
     def cleanup(self):
         """Cleanup caaspctl working environment"""
@@ -71,21 +57,20 @@ class Caaspctl:
             raise Exception("Failure(s) during cleanup")
 
     @step
-    def caaspctl_cluster_init(self):
+    def cluster_init(self):
         print("Cleaning up any previous test-cluster dir")
         self.utils.runshellcommand("rm {}/test-cluster -rf".format(self.conf.workspace))
         cmd = "cluster init --control-plane {} test-cluster".format(self._get_lb_ipaddr())
         self.utils.run_caaspctl(cmd, init=True)
 
     @step
-    def caaspctl_node_bootstrap(self):
+    def node_bootstrap(self):
         cmd = "node bootstrap --user {username} --sudo --target \
                  {ip} my-master-0".format(ip=self._get_masters_ipaddrs()[0], username=self.conf.nodeuser)
         self.utils.run_caaspctl(cmd)
-        self._num_master += 1
 
     @step
-    def _caaspctl_node_join(self, role="worker", nr=0):
+    def node_join(self, role="worker", nr=0):
         try:
             if role == "master":
                 ip_addr = self._get_masters_ipaddrs()[nr]
@@ -103,7 +88,7 @@ class Caaspctl:
             raise ("{}Error: {}{}".format(Constant.RED, cmd, Constant.RED_EXIT))
 
     @step
-    def _caaspctl_node_remove(self, role="worker", nr=0):
+    def node_remove(self, role="worker", nr=0):
         if nr <= 0:
             raise ("{}Error: there is not enough node to remove {} node in cluster{}".format(
                 Constant.RED, role,Constant.RED_EXIT))
@@ -119,76 +104,10 @@ class Caaspctl:
         except:
             raise ("{}Error: {}{}".format(Constant.RED, cmd, Constant.RED_EXIT))
 
-    @timeout(600)
-    @step
-    def add_worker_in_cluster(self):
-        try:
-            self._caaspctl_node_join(role="worker", nr=self._num_worker)
-            self._num_worker += 1
-        except:
-            self._num_worker -= 1
-
-    @timeout(600)
-    @step
-    def add_master_in_cluster(self):
-        try:
-            self._caaspctl_node_join(role="master", nr=self._num_master)
-            self._num_master += 1
-        except:
-            self._num_master -= 1
-
-    @timeout(600)
-    @step
-    def remove_worker_in_cluster(self):
-        try:
-            self._num_worker -= 1
-            self._caaspctl_node_remove(role="worker", nr=self._num_worker)
-        except:
-            self._num_worker += 1
-
-
-    @timeout(600)
-    @step
-    def remove_master_in_cluster(self):
-        try:
-            self._num_master -= 1
-            self._caaspctl_node_remove(role="master", nr=self._num_master)
-        except:
-            self._num_master += 1
-
-
-    @step
-    def add_nodes_in_cluster(self, num_master=1, num_worker=1):
-        cluster = Caaspctl(self.conf)
-
-        for _ in range(num_worker):
-            cluster.add_worker_in_cluster()
-        for _ in range(num_master):
-            cluster.add_master_in_cluster()
-
-        try:
-            cluster.caaspctl_cluster_status()
-        except:
-            pass
-
-    @step
-    def remove_nodes_in_cluster(self, num_master=0, num_worker=1):
-        cluster = Caaspctl(self.conf)
-
-        for _ in range(num_worker):
-            cluster.remove_worker_in_cluster()
-        for _ in range(num_master):
-            cluster.remove_master_in_cluster()
-
-        try:
-            cluster.caaspctl_cluster_status()
-        except:
-            pass
-
-    def caaspctl_cluster_status(self):
+    def cluster_status(self):
         self.utils.run_caaspctl("cluster status")
 
-    def _load_num_of_nodes(self):
+    def num_of_nodes(self):
         try:
             test_cluster = os.path.join(self.conf.workspace, "test-cluster")
             binpath = os.path.join(self.conf.workspace, 'go/bin/caaspctl')
