9da94924c6bb99f934eee538c01bd16bdc2445bc
Chang Lee <clee@suse.com>
2019-05-17 16:35:41 -0700

    Inital setup for Pylint check

--- ci/infra/testrunner/testrunner.py
+++ ci/infra/testrunner/testrunner.py
@@ -5,7 +5,7 @@
     This script can be run from Jenkins or manually, on developer desktops or servers.
 """
 
-import os, sys
+import sys, os
 from argparse import ArgumentParser
 
 testrunner_path = os.path.dirname(os.path.abspath(__file__))
@@ -22,24 +22,26 @@ __version__ = "0.0.3"
 
 
 def main():
-    help = """
+    '''Running testrunner.py through testrunner in python virtual env'''
+    comment = """
     This script is meant to be run manually on test servers, developer desktops, or Jenkins.
     This script supposed to run on python virtualenv from testrunner. Requires root privileges.
     Warning: it removes docker containers, VMs, images, and network configuration.
     """
-    parser = ArgumentParser(help)
+    parser = ArgumentParser(comment)
 
     parser.add_argument("-z", "--git-rebase", dest="git_rebase", action="store_true",
                         help="git rebase to master")
     parser.add_argument("-i", "--info", dest="ip_info", action='store_true', help='ip info')
     parser.add_argument("-x", "--cleanup", dest="cleanup", action='store_true',
-                          help="cleanup created caaspctl environment")
+                        help="cleanup created caaspctl environment")
     parser.add_argument("-t", "--terraform-apply", dest="apply_terraform", action="store_true",
-                        help="deploy nodes for cluster in your configured platform \
-                              e.g) openstack, vmware, ..")
+                        help="deploy nodes for cluster in your configured platform"
+                        "e.g) openstack, vmware, ..")
     parser.add_argument("-c", "--create-caaspctl", dest="create_caaspctl", action="store_true",
-                        help="create caaspctl environment {workspace}/go/src/github.com/SUSE/caaspctl\
-                              and build caaspctl in that directory")
+                        help="create caaspctl environment"
+                        "{workspace}/go/src/github.com/SUSE/caaspctl"
+                        " and build caaspctl in that directory")
     parser.add_argument("-b", "--bootstrap", dest="boostrap", action="store_true",
                         help="bootstrap k8s cluster with deployed nodes in your platform")
     parser.add_argument("-k", "--status", dest="cluster_status", action="store_true",
@@ -48,19 +50,20 @@ def main():
                         help="add nodes in k8s cluster. Default values are -m=1, -w=1")
     parser.add_argument("-r", "--remove-nodes", dest="remove_nodes", action="store_true",
                         help="remove nodes in k8s cluster. default values are -m=1, -w=1")
-    parser.add_argument("-l", "--log", dest="log", action="store_true", help="gather logs from nodes")
+    parser.add_argument("-l", "--log", dest="log", action="store_true",
+                        help="gather logs from nodes")
     parser.add_argument("-v", "--vars", dest="yaml_path", default="vars/openstack.yaml",
-                        help='path for platform yaml file. \
-                              Default is vars/openstack.yaml in {workspace}/ci/infra/testrunner. \
-                              eg) -v vars/myconfig.yaml')
+                        help="path for platform yaml file."
+                        " Default is vars/openstack.yaml in {workspace}/ci/infra/testrunner."
+                        " eg) -v vars/myconfig.yaml")
     parser.add_argument("-m", "--master", dest="num_master", type=int, default=1,
-                        help='number of masters to add or delete. It is dependening on \
-                              number of deployed master nodes in your yaml file. Default value is 1. \
-                              eg) -m 2')
+                        help="number of masters to add or delete. It is dependening on"
+                        " number of deployed master nodes in your yaml file. Default value is 1."
+                        " eg) -m 2")
     parser.add_argument("-w", "--worker", dest="num_worker", type=int, default=1,
-                        help='number of workers to add or delete. It is dependening on \
-                              number of deployed worker nodes in your yaml file. Default value is 1  \
-                              eg) -w 2')
+                        help="number of workers to add or delete. It is dependening on"
+                        " number of deployed worker nodes in your yaml file. Default value is 1"
+                        " eg) -w 2")
 
     options = parser.parse_args()
     conf = BaseConfig(options.yaml_path)
@@ -99,9 +102,11 @@ def main():
     elif options.cluster_status:
         Caaspctl(conf).caaspctl_cluster_status()
     elif options.add_nodes:
-        Caaspctl(conf).add_nodes_in_cluster(num_master=options.num_master, num_worker=options.num_worker)
+        Caaspctl(conf).add_nodes_in_cluster(num_master=options.num_master,
+                                            num_worker=options.num_worker)
     elif options.remove_nodes:
-        Caaspctl(conf).remove_nodes_in_cluster(num_master=options.num_master, num_worker=options.num_worker)
+        Caaspctl(conf).remove_nodes_in_cluster(num_master=options.num_master,
+                                               num_worker=options.num_worker)
     elif options.log:
         Caaspctl(conf).gather_logs()
 
