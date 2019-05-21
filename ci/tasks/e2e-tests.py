cdface7640931b04659a8f74c135bd3d8329b772
dmaiocchi <dmaiocchi@suse.com>
2019-05-21 17:30:18 +0200

    Refactor e2e-tests
    Raise user custom exception and remove unecessary check.
    If ginkgo isn't installed an exception will be throwed

--- ci/tasks/e2e-tests.py
+++ ci/tasks/e2e-tests.py
@@ -8,19 +8,13 @@ import os
 import json
 import sys
 
-try:
-  subprocess.check_call("ginkgo")
-except Exception  as ex:
-  print("Please install - ginkgo - before!")
-  sys.exit(1)
-  
 # 1) set all IPS variable individually ( with the env. variables)
 # 2) read this IPS from a tfstate which will set the env. variables.
 
 if os.environ.get('IP_FROM_TF_STATE') == 'True' or 'TRUE':
   # we need to know which provider is beeing used to read the tfstates.
   if not "PLATFORM" in os.environ:
-    raise("you need to set PLATFORM ENV. variable with LOAD_IP_FROM_TF_STATE env var")
+    raise(Exception("you need to set PLATFORM ENV. variable with LOAD_IP_FROM_TF_STATE env var"))
 
   tf = os.path.join("ci/infra/{0}".format(os.environ['PLATFORM'].lower()), "terraform.tfstate")
   with open(tf) as f:
@@ -29,4 +23,8 @@ if os.environ.get('IP_FROM_TF_STATE') == 'True' or 'TRUE':
   os.environ['MASTER00'] = tf_state["modules"][0]["outputs"]["ip_masters"]["value"][0]
   os.environ['WORKER00'] = tf_state["modules"][0]["outputs"]["ip_workers"]["value"][0]
 
-subprocess.check_call("cd test && ginkgo -v --race --trace --progress core-features", shell=True, env=dict(os.environ))
+try:
+  subprocess.check_call("cd test && ginkgo -v --race --trace --progress core-features", shell=True, env=dict(os.environ))
+except Exception as ex:
+     print(ex)
+     sys.exit(2)
