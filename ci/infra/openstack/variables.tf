db101e191659eabde7a02ffaa2b97ad12b981c04
Panagiotis Georgiadis <pgeorgiadis@suse.com>
2019-05-21 15:50:18 +0200

    Remove hardcoded values for openstack deployment
    Remove the default hardcoded values for openstack deployment found
    into variables.tf. Offer those as part of the example file
    terraform.tfvars.example instead.
    
    Remove the example for adding new packages. This will create a
    problem since the current implementation is not appending the
    packages list but overwriting. As a result if we give this
    freedom, the users might be able to shoot themselves in the foot
    by not including the required packages for caaspctl.

--- ci/infra/openstack/variables.tf
+++ ci/infra/openstack/variables.tf
@@ -1,64 +1,52 @@
 variable "image_name" {
-  default     = "openSUSE-Leap-15.0-OpenStack.x86_64"
+  default     = ""
   description = "Name of the image to use"
 }
 
 variable "repositories" {
   type = "list"
-
-  default = [
-    {
-      caasp_devel_leap15 = "https://download.opensuse.org/repositories/devel:/CaaSP:/Head:/ControllerNode/openSUSE_Leap_15.0"
-    },
-  ]
-
+  default = []
   description = "Urls of the repositories to mount via cloud-init"
 }
 
 variable "internal_net" {
-  default     = "testing-net"
+  default     = ""
   description = "Name of the internal network to be created"
 }
 
 variable "subnet_cidr" {
-  default     = "172.28.0.0/24"
+  default     = ""
   description = "CIDR of the subnet for the internal network"
 }
 
 variable "dns_nameservers" {
   type = "list"
-
-  default = [
-    "172.28.0.2",
-    "8.8.8.8",
-    "8.8.8.4",
-  ]
-
+  default = []
   description = "DNS servers for the nodes"
 }
 
 variable "external_net" {
-  default     = "floating"
+  default     = ""
   description = "Name of the external network to be used, the one used to allocate floating IPs"
 }
 
 variable "master_size" {
-  default     = "m1.medium"
+  default     = ""
   description = "Size of the master nodes"
 }
 
 variable "masters" {
-  default     = 1
+  default     = 0
   description = "Number of master nodes"
 }
 
 variable "worker_size" {
-  default     = "m1.medium"
+  default     = ""
   description = "Size of the worker nodes"
 }
 
 variable "workers" {
-  default     = 1
+  default     = 0
   description = "Number of worker nodes"
 }
 
@@ -68,35 +56,35 @@ variable "workers_vol_enabled" {
 }
 
 variable "workers_vol_size" {
-  default     = 5
-  description = "size of the volumes in GB"
+  default     = 0
+  description = "Size of the worker volumes in GB"
 }
 
 variable "dnsdomain" {
-  default     = "testing.qa.caasp.suse.net"
-  description = "TBD - leftover?"
+  default     = ""
+  description = "Name of DNS domain"
 }
 
 variable "dnsentry" {
   default     = 0
-  description = "TBD - leftover?"
+  description = "DNS Entry"
 }
 
 variable "stack_name" {
-  default     = "testing"
-  description = "identifier to make all your resources unique and avoid clashes with other users of this terraform project"
+  default     = ""
+  description = "Identifier to make all your resources unique and avoid clashes with other users of this terraform project"
 }
 
 variable "authorized_keys" {
   type        = "list"
   default     = []
-  description = "ssh keys to inject into all the nodes"
+  description = "SSH keys to inject into all the nodes"
 }
 
 variable "ntp_servers" {
   type        = "list"
   default     = []
-  description = "list of ntp servers to configure"
+  description = "List of ntp servers to configure"
 }
 
 variable "packages" {
@@ -104,25 +92,19 @@ variable "packages" {
 
   default = [
     "kubernetes-kubeadm",
-    "kubernetes-kubelet",
     "kubernetes-client",
-    "cri-o",
-    "cni-plugins",
-    "-docker",
-    "-containerd",
-    "-docker-runc",
-    "-docker-libnetwork",
+    "cri-o"
   ]
 
-  description = "list of additional packages to install"
+  description = "List of required packages to install"
 }
 
 variable "username" {
-  default     = "opensuse"
+  default     = ""
   description = "Username for the cluster nodes"
 }
 
 variable "password" {
-  default     = "linux"
+  default     = ""
   description = "Password for the cluster nodes"
 }
