7e0102bbf2d15bdf4440a27367e6c05be90686b4
David Cassany <dcassany@suse.de>
2019-05-29 13:03:16 +0200

    Add packaging helpers
    This commit adds helpers in `ci/packaging/suse` folder to facilitate
    the creation of the files used in OBS to package skuba-update as an
    RPM. This commit also includes a Makefile with the `suse-package`
    target that results with the files required by skuba-update package
    in OBS.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 
 from setuptools import setup
 
-
 setup(
     name = "skuba-update",
     version = '0.1.0',
     author = "SUSE Containers Team",
     author_email = "containers@suse.com",
-    description = "Utility to automatically refresh and update a SUSE CaaSP cluster",
+    description = "Utility to automatically refresh and update a SUSE CaaSP cluster nodes",
+    long_description = "Wraps zypper to refresh repositories and apply non interactive patches, including patches requiring reboot.",
     license = "Apache License 2.0",
     keywords = "CaaSP",
     url = "https://github.com/SUSE/skuba-update",
@@ -19,7 +19,7 @@ setup(
         'License :: OSI Approved :: Apache License 2.0',
         'Operating System :: POSIX :: Linux',
     ],
-    data_files={'skuba_update/spec.template'},
+    #data_files={'ci/packaging/suse/skuba-update_spec.tmpl'},
     entry_points = {
         'console_scripts': [
             'skuba-update = skuba_update.skuba_update:main'
