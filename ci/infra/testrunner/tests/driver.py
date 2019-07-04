0185a0ee2fa9ac50d3a3b609f420a3bf0ec9c9d7
Pablo Chacin <pchacin@suse.com>
2019-07-04 19:10:10 +0200

    Implement test command using pytest
    Implement test command by calling the pytest
    testing framework, passing parameters from the
    command line.
    
    Remove tests that used previous implementation.
    
    Signed-off-by: Pablo Chacin <pchacin@suse.com>

---
+++ ci/infra/testrunner/tests/driver.py
@@ -0,0 +1,39 @@
+import os
+import pytest 
+
+class PyTestOpts:
+
+    SHOW_OUTPUT = "-s"
+
+    COLLECT_TESTS = "--collect-only"
+
+class TestDriver:
+
+
+    def __init__(self, conf):
+        self.conf = conf
+        
+    def run(self, module="tests", test_suite=None, test=None, verbose=False, collect=False):
+        
+        opts = []
+        
+        if verbose:
+            opts.append(PyTestOpts.SHOW_OUTPUT)
+
+        if collect:
+            opts.append(PyTestOpts.COLLECT_TESTS)
+
+        test_path = module
+
+        if test_suite:
+            if not test_suite.endswith(".py"):
+                raise ValueError("Test suite must be a python file")
+            test_path = os.path.join(module,test_suite)
+        
+        if test:
+            test_path += "{}::{}".format(test_path, test)
+
+        # Path must be the last argument
+        opts.append(test_path)
+
+        pytest.main(args=opts) 
