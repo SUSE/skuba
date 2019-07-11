96187dfadcd5f7698b0af6aacc094cbcc6467d92
Vicente Zepeda Mas <vzepedamas@suse.com>
2019-07-11 15:05:15 +0200

    adds more functionality to kubectl, for deploying services
    Signed-off-by: Vicente Zepeda Mas <vzepedamas@suse.com>

--- ci/infra/testrunner/kubectl/kubectl.py
+++ ci/infra/testrunner/kubectl/kubectl.py
@@ -20,3 +20,58 @@ class Kubectl:
             utils.runshellcommand("kubectl get nodes --kubeconfig={cwd}/test-cluster/admin.conf".format(cwd=conf.workspace))
         except Exception as ex:
             print("Received the following error {}".format(ex))
+
+    @staticmethod
+    def get_pods(conf):
+        print("gets pods using kubectl")
+        utils = Utils(conf)
+        try:
+            utils.runshellcommand("kubectl get po --kubeconfig={cwd}/test-cluster/admin.conf -o wide".format(cwd=conf.workspace))
+        except Exception as ex:
+            print("Received the following error {}".format(ex))
+
+    @staticmethod
+    def create_deployment(conf, name, image):
+        print("create a new deployment")
+        utils = Utils(conf)
+        try:
+            utils.runshellcommand("kubectl create deployment {name} --kubeconfig={cwd}/test-cluster/admin.conf --image={image}".format(cwd=conf.workspace, name=name, image=image))
+        except Exception as ex:
+            print("Received the following error {}".format(ex))
+
+    @staticmethod
+    def scale_deployment(conf, name, replicas):
+        print("scale a deployment")
+        utils = Utils(conf)
+        try:
+            utils.runshellcommand("kubectl scale deployment {name} --kubeconfig={cwd}/test-cluster/admin.conf --replicas={replicas}".format(cwd=conf.workspace, name=name, replicas=replicas))
+        except Exception as ex:
+            print("Received the following error {}".format(ex))
+
+    @staticmethod
+    def expose_deployment(conf, name, port, nodeType="NodePort"):
+        print("expose a deployment")
+        utils = Utils(conf)
+        try:
+            utils.runshellcommand("kubectl expose deployment {name} --kubeconfig={cwd}/test-cluster/admin.conf --port={port} --type={nodeType}".format(cwd=conf.workspace, name=name, port=port, nodeType=nodeType))
+        except Exception as ex:
+            print("Received the following error {}".format(ex))
+
+    @staticmethod
+    def wait_deployment(conf, name, timeout):
+        print("wait a deployment")
+        utils = Utils(conf)
+        try:
+            utils.runshellcommand("kubectl wait --for=condition=available deploy/{name} --kubeconfig={cwd}/test-cluster/admin.conf --timeout={timeout}m".format(cwd=conf.workspace, name=name, timeout=timeout))
+        except Exception as ex:
+            print("Received the following error {}".format(ex))
+
+    @staticmethod
+    def count_available_replicas(conf, name):
+        print("count available replicas of a deployment")
+        utils = Utils(conf)
+        try:
+            return int(utils.runshellcommand_withoutput("kubectl get deployment/{name} -o json --kubeconfig={cwd}/test-cluster/admin.conf | jq '.status.availableReplicas'".format(cwd=conf.workspace, name=name), False))
+        except Exception as ex:
+            print("Received the following error {}".format(ex))
+            return 0
--- ci/infra/testrunner/tests/test_workers.py
+++ ci/infra/testrunner/tests/test_workers.py
@@ -8,9 +8,9 @@ import time
 def setup(request, conf):
     platform = Platform.get_platform(conf) 
     platform.provision()
-    def cleanup():
-        platform.cleanup()
-    request.addfinalizer(cleanup)
+    # def cleanup():
+    #     platform.cleanup()
+    # request.addfinalizer(cleanup)
 
     skuba = Skuba(conf)
     skuba.cluster_init()
@@ -22,14 +22,22 @@ def test_worker(setup, conf):
     skuba = Skuba(conf)
     kubectl = Kubectl(conf)
     skuba.node_join(role="worker", nr=0)
+    skuba.node_join(role="worker", nr=1)
     masters = skuba.num_of_nodes("master")
     workers = skuba.num_of_nodes("worker")
     assert masters == 1
-    assert workers == 1
-    skuba.node_remove(role="worker", nr=0)
-    masters = skuba.num_of_nodes("master")
-    workers = skuba.num_of_nodes("worker")
-    assert masters == 1
-    assert workers == 0
+    assert workers == 2
+    # skuba.node_remove(role="worker", nr=0)
+    # masters = skuba.num_of_nodes("master")
+    # workers = skuba.num_of_nodes("worker")
+    # assert masters == 1
+    # assert workers == 0
     kubectl.get_nodes(conf)
+    kubectl.get_pods(conf)
+    kubectl.create_deployment(conf, "nginx", "nginx:stable-alpine")
+    kubectl.scale_deployment(conf, "nginx", workers)
+    kubectl.expose_deployment(conf, "nginx", 80)
+    kubectl.wait_deployment(conf, "nginx", 3)
+    kubectl.get_pods(conf)
+    assert kubectl.count_available_replicas(conf, "nginx") == workers
 
