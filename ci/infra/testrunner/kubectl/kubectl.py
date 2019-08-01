7dfaaf9b1691da9e53b3f1bee71e5ff7c0974bfa
Pablo Chacin <pchacin@suse.com>
2019-08-01 18:40:55 +0200

    Remove unnecessary try blocks
    Signed-off-by: Pablo Chacin <pchacin@suse.com>

--- ci/infra/testrunner/kubectl/kubectl.py
+++ ci/infra/testrunner/kubectl/kubectl.py
@@ -16,53 +16,30 @@ class Kubectl:
 
 
     def create_deployment(self, name, image):
-        try:
-            self._run_kubectl("create deployment {name} --image={image}"
-                                .format(name=name, image=image))
-        except Exception as ex:
-            raise Exception("Error executing cmd {}") from ex
-
+        self._run_kubectl("create deployment {name} --image={image}"
+                          .format(name=name, image=image))
 
     def scale_deployment(self, name, replicas):
-        try:
-            self._run_kubectl("scale deployment {name} --replicas={replicas}"
-                             .format(name=name, replicas=replicas))
-        except Exception as ex:
-            raise Exception("Error executing cmd {}") from ex
-
+        self._run_kubectl("scale deployment {name} --replicas={replicas}"
+                          .format(name=name, replicas=replicas))
 
     def expose_deployment(self, name, port, nodeType="NodePort"):
-        try:
-            self._run_kubectl("expose deployment {name} --port={port} --type={nodeType}"
-                             .format(name=name, port=port, nodeType=nodeType))
-        except Exception as ex:
-            raise Exception("Error executing cmd {}") from ex
-
+        self._run_kubectl("expose deployment {name} --port={port} --type={nodeType}"
+                          .format(name=name, port=port, nodeType=nodeType))
 
     def wait_deployment(self, name, timeout):
-        try:
-            self._run_kubectl("wait --for=condition=available deploy/{name} --timeout={timeout}m"
-                             .format(name=name, timeout=timeout))
-        except Exception as ex:
-            raise Exception("Error executing cmd {}") from ex
-
+        self._run_kubectl("wait --for=condition=available deploy/{name} --timeout={timeout}m"
+                          .format(name=name, timeout=timeout))
 
     def count_available_replicas(self, name):
-        try:
-            result = self._run_kubectl("get deployment/{name} | jq '.status.availableReplicas'"
-                                       .format(name=name))
-            return int(result)
-        except Exception as ex:
-            raise Exception("Error executing cmd {}") from ex
-
+        result = self._run_kubectl("get deployment/{name} | jq '.status.availableReplicas'"
+                                   .format(name=name))
+        return int(result)
 
     def get_service_port(self, name):
-        try:
-            result = self._run_kubectl("get service/{name} | jq '.spec.ports[0].nodePort'"
-                                       .format(name=name))
-            return int(result)
-        except Exception as ex:
-            raise Exception("Error executing cmd {}") from ex
+        result = self._run_kubectl("get service/{name} | jq '.spec.ports[0].nodePort'"
+                                   .format(name=name))
+        return int(result)
 
     def test_service(self, name, path="/", worker=0):
         ip_addresses = self.platform.get_nodes_ipaddrs("worker")
@@ -81,7 +58,7 @@ class Kubectl:
         shell_cmd = "kubectl --kubeconfig={cwd}/test-cluster/admin.conf \
                       -o json {command}".format(command=command, cwd=self.conf.workspace)
         try:
-            return self.utils.runshellcommand(shell_cmd, output=True)
+            return self.utils.runshellcommand(shell_cmd)
         except Exception as ex:
             raise Exception("Error executing cmd {}".format(shell_cmd)) from ex
 
