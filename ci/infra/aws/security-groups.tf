45ab715e05d332d55e1f49f7436f17d09bfa713b
Flavio Castelli <fcastelli@suse.com>
2019-08-02 12:52:38 +0200

    aws security rules: expose port 80 on elb
    Ensure the load balancer we provision allows ingress traffic on port 80.

--- ci/infra/aws/security-groups.tf
+++ ci/infra/aws/security-groups.tf
@@ -196,6 +196,14 @@ resource "aws_security_group" "elb" {
 
   # HTTP access from anywhere
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS access from anywhere
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
