7850a3f1ce667d122e9dc8d0d5bf415ffc345760
Ty Daines <tdaines@suse.com>
2019-08-21 19:21:21 -0700

    Add worker and master removal tests

---
+++ ci/infra/testrunner/tests/test_masters.py
@@ -0,0 +1,13 @@
+import pytest
+from tests.utils import wait
+
+
+@pytest.mark.disruptive
+def test_remove_master(deployment, conf, platform, skuba, kubectl):
+    initial_masters = skuba.num_of_nodes("master")
+
+    # Remove the master
+    skuba.node_remove(role="master", nr=initial_masters - 1)
+    assert skuba.num_of_nodes("master") == initial_masters - 1
+
+    wait(kubectl.run_kubectl, 'wait --timeout=5m --for=condition=Ready pods --all --namespace=kube-system', wait_delay=60, wait_timeout=300, wait_backoff=30, wait_retries=5, wait_allow=(RuntimeError))
