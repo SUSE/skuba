d0aa67bfbe734a42b8c9088fdae715b39fc8d4b7
Rafael Fernández López <rfernandezlopez@suse.com>
2019-08-28 13:31:01 +0200

    Fix tag detection
    The previous approach for detecting if we were in a tag was not
    supporting annotated tags. With this method we are supporting both
    lightweight and annotated tags.
    
    Signed-off-by: Rafael Fernández López <rfernandezlopez@suse.com>

--- ci/packaging/suse/rpmfiles_maker.sh
+++ ci/packaging/suse/rpmfiles_maker.sh
@@ -7,13 +7,13 @@ tmp_dir=$(mktemp -d -t skuba_XXXX)
 rpm_files="${susepkg_dir}/obs_files"
 version="$1"
 tag="$2"
-annotated_tag="$3"
+closest_tag="$3"
 
 log()   { (>&2 echo ">>> $*") ; }
 clean() { log "Cleaning temporary directory ${tmp_dir}"; rm -rf "${tmp_dir}"; }
 
 if [ $# -ne 3 ]; then
-    log "usage: <version> <tag> <annotated-tag>"
+    log "usage: <version> <tag> <closest-tag>"
     exit 1
 fi
 
@@ -22,7 +22,7 @@ trap clean ERR
 rm -rf "${rpm_files}"
 mkdir -p "${rpm_files}"
 git archive --prefix=skuba/ -o "${tmp_dir}/skuba.tar.gz" HEAD
-sed -e "s|%%VERSION|${version}|;s|%%TAG|${tag}|;s|%%ANNOTATED_TAG|${annotated_tag}|" "${susepkg_dir}/skuba_spec_template" \
+sed -e "s|%%VERSION|${version}|;s|%%TAG|${tag}|;s|%%CLOSEST_TAG|${closest_tag}|" "${susepkg_dir}/skuba_spec_template" \
     > "${tmp_dir}/skuba.spec"
 make CHANGES="${tmp_dir}/skuba.changes.append" suse-changelog
 cp "${tmp_dir}"/* "${rpm_files}"
