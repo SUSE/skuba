c50deed2c97993d8f0b51c0cfb25fa90defe090f
Maximilian Meister <mmeister@suse.de>
2019-08-30 08:42:25 +0200

    Add new upgrade e2e test
    We have to make sure that skuba behaves correctly in case of
    a node upgrade plan on a worker, when a control plane is already
    updated.
    
    This has not been covered yet in the node upgrade plan
    test and is better to keep separated due to the different
    environment.
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

--- ci/infra/testrunner/tests/test_skuba_upgrade.py
+++ ci/infra/testrunner/tests/test_skuba_upgrade.py
@@ -101,6 +101,33 @@ def test_upgrade_plan_from_previous(setup, skuba, kubectl, platform):
 
 
 @pytest.mark.disruptive
+def test_upgrade_plan_from_previous_with_upgraded_control_plane(setup, skuba, kubectl, platform):
+    """
+    Starting from an updated control plane, check what cluster/node plan report.
+    """
+
+    setup_kubernetes_version(platform, skuba, kubectl, PREVIOUS_VERSION)
+
+    masters = platform.get_num_nodes("master")
+    for n in range (0, masters):
+        node = "my-master-{}".format(n)
+        assert node_is_ready(kubectl, node)
+        master = skuba.node_upgrade("apply", "master", n)
+        assert master.find("successfully upgraded") != -1
+        assert node_is_upgraded(kubectl, platform, node)
+
+    workers = platform.get_num_nodes("worker")
+    for n in range (0, workers):
+        worker = skuba.node_upgrade("plan", "worker", n)
+        assert worker.find(
+            "Current Kubernetes cluster version: {pv}".format(pv=PREVIOUS_VERSION))
+        assert worker.find("Latest Kubernetes version: {cv}".format(
+            cv=CURRENT_VERSION)) != -1
+        assert worker.find(" - kubelet: {pv} -> {cv}".format(
+            pv=PREVIOUS_VERSION, cv=CURRENT_VERSION)) != -1
+
+
+@pytest.mark.disruptive
 def test_upgrade_apply_all_fine(setup, platform, skuba, kubectl):
     """
     Starting from a up-to-date cluster, check what node upgrade apply reports.
