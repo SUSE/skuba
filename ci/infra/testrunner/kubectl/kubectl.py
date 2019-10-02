820daa06f482ea81c6e86c647505077775d615d4
David Cassany <dcassany@suse.com>
2019-10-02 15:37:44 +0200

    Add e2e test for skuba addon upgrades
    This commits adds e2e tests that:
    
    1 deploys a cluster
    2 verifies it is up to date
    3 manually decreases addons version of the deployed cluster
    4 verifies skuba detects available updates
    5 upgrades the addons
    6 verifies the cluster is up to date again

--- ci/infra/testrunner/kubectl/kubectl.py
+++ ci/infra/testrunner/kubectl/kubectl.py
@@ -12,11 +12,11 @@ class Kubectl:
         self.platform = platforms.get_platform(conf, platform)
         self.skuba = Skuba(conf, platform)
 
-    def run_kubectl(self, command):
+    def run_kubectl(self, command, stdin=None):
         kubeconfig = self.skuba.get_kubeconfig()
 
         shell_cmd = "kubectl --kubeconfig={} {}".format(kubeconfig, command)
         try:
-            return self.utils.runshellcommand(shell_cmd)
+            return self.utils.runshellcommand(shell_cmd, stdin=stdin)
         except Exception as ex:
             raise Exception("Error executing cmd {}".format(shell_cmd)) from ex
---
+++ ci/infra/testrunner/tests/test_skuba_addon_upgrade.py
@@ -0,0 +1,90 @@
+import pytest
+import yaml
+import re
+from io import StringIO
+
+
+def replace_skuba_config(kubectl, configmap_data):
+    new_configmap = kubectl.run_kubectl(
+        'create configmap skuba-config --from-literal {0} -o yaml --namespace kube-system --dry-run'.format(
+            configmap_data
+        )
+    )
+    return kubectl.run_kubectl("replace -f -", stdin=new_configmap.encode())
+
+def get_skubaConfiguration_dict(kubectl):
+    skubaConf_yml = kubectl.run_kubectl(
+        "get configmap skuba-config --namespace kube-system -o jsonpath='{.data.SkubaConfiguration}'"
+    )
+    return yaml.load(skubaConf_yml, Loader=yaml.FullLoader)
+
+def decrease_one_addon_manifest(addons_dict, skip=None):
+    for addon in addons_dict:
+        if addon == skip:
+            continue
+        ver = addons_dict[addon]['ManifestVersion'] 
+        if ver > 0:
+            addons_dict[addon]['ManifestVersion'] -= 1
+            return (
+                addon, addons_dict[addon]['Version'], (ver - 1, ver)
+            )
+    raise Exception("Could not decrease any addon manifest version!")
+
+def decrease_one_addon_image(addons_dict, skip=None):
+    for addon in addons_dict:
+        if addon == skip:
+            continue
+        cur_ver = addons_dict[addon]['Version']
+        addons_dict[addon]['Version'] = '0.0.1'
+        return (
+            addon, ('0.0.1', cur_ver), addons_dict[addon]['ManifestVersion']
+        )
+    raise Exception("Could not decrease any addon image version!")
+
+def addons_up_to_date(skuba):
+    all_fine = re.compile(
+        r'Congratulations! Addons for \d+\.\d+\.\d+ are already at the latest version available'
+    )
+    out = skuba.addon_upgrade('plan')
+    return bool(all_fine.findall(out))
+
+def test_addon_upgrade_plan(deployment, kubectl, skuba):
+    assert addons_up_to_date(skuba)
+
+    skubaConf_dict = get_skubaConfiguration_dict(kubectl)
+    addons_dict = skubaConf_dict['AddonsVersion']
+
+    u_manif = decrease_one_addon_manifest(addons_dict)
+    u_img = decrease_one_addon_image(addons_dict, skip=u_manif[0])
+
+    u_img_msg = '{0}: {1} -> {2}'.format(u_img[0], u_img[1][0], u_img[1][1])
+    u_manif_msg = '{0}: {1} -> {1} (manifest version from {2} to {3})'.format(
+        u_manif[0], u_manif[1], u_manif[2][0], u_manif[2][1]
+    )
+
+    out = replace_skuba_config(
+        kubectl, "SkubaConfiguration='{0}'".format(yaml.dump(skubaConf_dict))
+    )
+    assert out.find("configmap/skuba-config replaced") != -1
+
+    out = skuba.addon_upgrade('plan')
+    assert out.find(u_manif_msg) != -1
+    assert out.find(u_img_msg) != -1
+
+def test_addon_upgrade_apply(deployment, kubectl, skuba):
+    skubaConf_dict = get_skubaConfiguration_dict(kubectl)
+    addons_dict = skubaConf_dict['AddonsVersion']
+
+    decrease_one_addon_image(addons_dict)
+    decrease_one_addon_manifest(addons_dict)
+
+    out = replace_skuba_config(
+        kubectl, "SkubaConfiguration='{0}'".format(yaml.dump(skubaConf_dict))
+    )
+    assert out.find("configmap/skuba-config replaced") != -1
+
+    assert not addons_up_to_date(skuba)
+    
+    out = skuba.addon_upgrade('apply')
+    assert out.find("Successfully upgraded addons") != -1
+    assert addons_up_to_date(skuba)
