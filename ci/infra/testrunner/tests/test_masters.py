a9ccecdc1633e9415959b826ca06d68c382308a6
Ty Daines <tdaines@suse.com>
2019-10-10 11:54:58 -0700

    Don't check status of completed pods
    Removing a master leaves behind a completed pod that will never be ready

--- ci/infra/testrunner/tests/test_masters.py
+++ ci/infra/testrunner/tests/test_masters.py
@@ -10,4 +10,4 @@ def test_remove_master(deployment, conf, platform, skuba, kubectl):
     skuba.node_remove(role="master", nr=initial_masters - 1)
     assert skuba.num_of_nodes("master") == initial_masters - 1
 
-    wait(kubectl.run_kubectl, 'wait --timeout=5m --for=condition=Ready pods --all --namespace=kube-system', wait_delay=60, wait_timeout=300, wait_backoff=30, wait_retries=5, wait_allow=(RuntimeError))
+    wait(kubectl.run_kubectl, 'wait --timeout=5m --for=condition=Ready pods --all --namespace=kube-system  --field-selector=status.phase!=Succeeded', wait_delay=60, wait_timeout=300, wait_backoff=30, wait_retries=5, wait_allow=(RuntimeError))
