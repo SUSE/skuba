3a139d5c3111a3ed62934c70c8cabb35310ded26
Chang Lee <clee@suse.com>
2019-11-01 10:15:56 -0700

    Add K8s interface for Cmd Addon Upgrade
    Adding an interface between GetAdminClientSet & Skuba allows
    substituting a mock implementation that can return a fake ClientSet.

--- cmd/skuba/addon/upgrade.go
+++ cmd/skuba/addon/upgrade.go
@@ -22,7 +22,9 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"k8s.io/klog"
 
+	"github.com/SUSE/skuba/internal/pkg/skuba/kubernetes"
 	addons "github.com/SUSE/skuba/pkg/skuba/actions/addon/upgrade"
 )
 
@@ -46,7 +48,12 @@ func newUpgradePlanCmd() *cobra.Command {
 		Use:   "plan",
 		Short: "Plan addon upgrade",
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := addons.Plan(); err != nil {
+			clientSet, err := kubernetes.GetAdminClientSet()
+			if err != nil {
+				klog.Errorf("unable to get admin client set: %s", err)
+				os.Exit(1)
+			}
+			if err := addons.Plan(clientSet); err != nil {
 				fmt.Printf("Unable to plan addon upgrade: %s\n", err)
 				os.Exit(1)
 			}
@@ -60,7 +67,12 @@ func newUpgradeApplyCmd() *cobra.Command {
 		Use:   "apply",
 		Short: "Apply addon upgrade",
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := addons.Apply(); err != nil {
+			clientSet, err := kubernetes.GetAdminClientSet()
+			if err != nil {
+				klog.Errorf("unable to get admin client set: %s", err)
+				os.Exit(1)
+			}
+			if err := addons.Apply(clientSet); err != nil {
 				fmt.Printf("Unable to Apply addons upgrade: %s\n", err)
 				os.Exit(1)
 			}
