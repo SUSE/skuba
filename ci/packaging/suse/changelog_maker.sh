e4a63af99b8dfe33275391c236492ad42e07db41
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2019-11-20 11:26:15 +0100

    [Release] check tags from multiple branches
    Initially we were releasing only from master branch.
    However, in order not to block master branch, we are now also releasing
    from branches called release-caasp-x.y.z
    
    The script that was creating the changelog was taking commits that have
    been merged into the master branch and so it was including entries that
    have already been released.
    
    Removing the "--merge" option from the git command will take into
    account all versions.
    
    Given we are not maintaining more than one version, this will work.
    
    In the other hand, we need to remove the cherry-picked commits
    
    "git log v1..v2" is not the same as "git log v1...v2" (note the 3 dots)
    
    If we use the second form, then we can use "git log --cherry-pick" to
    remove cherry-picked commits
    
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>
    (cherry picked from commit 65e3800cf85907b86afe30e0aa06b929ae90e43e)

--- ci/packaging/suse/changelog_maker.sh
+++ ci/packaging/suse/changelog_maker.sh
@@ -7,13 +7,13 @@ changes=${1:-skuba.changes.append}
 
 [ -f "${changes}" ] && rm "${changes}"
 
-mapfile -t tags < <( git tag --merged | sort -rV | head -n2 )
-scope="${tags[1]}..${tags[0]}"
+mapfile -t tags < <( git tag | sort -rV | head -n2 )
+scope="${tags[1]}...${tags[0]}"
 
 {
     git show --format="${header}%n%n- Update to ${tags[0]}:" \
         --date="format-local:${datef}" -s "${tags[0]}^{commit}"
-    git log -s --format="%w(77,2,12)* %h %s" --no-merges "${scope}"
+    git log -s --cherry-pick --format="%w(77,2,12)* %h %s" --no-merges "${scope}"
     # Add empty line
     echo
 } >> "${changes}"
