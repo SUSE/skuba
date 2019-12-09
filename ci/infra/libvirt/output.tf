ca4f59cdc793bac1999bcf9573b3176cdc970658
Michal Rostecki <mrostecki@suse.de>
2019-12-10 00:09:34 +0100

    libvirt: Add hostnames to the output
    The last argument of `skuba node` subcommands should be the same as
    hostname of the provisioned node, otherwise kubeadm is going to show the
    warning and the cluster might be not fully functional.
    
    Before this change, looking up for hostnames after executing terraform
    required manual lookup in libvirt domain data. This change adds
    hostnames to the output of `terraform apply` command.
    
    Signed-off-by: Michal Rostecki <mrostecki@suse.de>

--- ci/infra/libvirt/output.tf
+++ ci/infra/libvirt/output.tf
@@ -1,3 +1,11 @@
+output "hostnames_masters" {
+  value = ["${libvirt_domain.master.*.network_interface.0.hostname}"]
+}
+
+output "hostnames_workers" {
+  value = ["${libvirt_domain.worker.*.network_interface.0.hostname}"]
+}
+
 output "ip_load_balancer" {
   value = "${libvirt_domain.lb.network_interface.0.addresses.0}"
 }
