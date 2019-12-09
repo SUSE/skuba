fa21bc590591e8d21586b052c00d56a663f24766
JenTing Hsiao <jenting.hsiao@suse.com>
2019-12-09 19:37:22 +0800

    Fix unable to bootstrap by using FQDN (#886)
    Signed-off-by: JenTing Hsiao <jenting.hsiao@suse.com>

--- internal/pkg/skuba/deployments/ssh/kubelet.go
+++ internal/pkg/skuba/deployments/ssh/kubelet.go
@@ -85,8 +85,13 @@ func kubeletCreateAndUploadServerCert(t *Target, data interface{}) error {
 		}
 	}
 
-	alternateIPs := []net.IP{net.ParseIP(t.target.Target), net.IPv4(127, 0, 0, 1), net.IPv6loopback}
+	alternateIPs := []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback}
 	alternateDNS := []string{"localhost"}
+	if ip := net.ParseIP(t.target.Target); ip != nil {
+		alternateIPs = append(alternateIPs, ip)
+	} else {
+		alternateDNS = append(alternateDNS, t.target.Target)
+	}
 
 	altNames.IPs = append(altNames.IPs, alternateIPs...)
 	altNames.DNSNames = append(altNames.DNSNames, alternateDNS...)
