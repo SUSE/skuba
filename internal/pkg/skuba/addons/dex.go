59e19939b6766396e2749e59f2b0d5d9e3b3a782
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2020-01-27 16:36:06 +0100

    Revert "fixes bsc#1157338 (#860)"
    This reverts commit 55026300ec1317a108003c643b69ae5db61972af.
    
    Because of https://github.com/SUSE/avant-garde/issues/1257 we need to
    revert https://github.com/SUSE/skuba/pull/860

--- internal/pkg/skuba/addons/dex.go
+++ internal/pkg/skuba/addons/dex.go
@@ -161,12 +161,8 @@ kind: Deployment
 metadata:
   name: oidc-dex
   namespace: kube-system
-  labels:
-    app: oidc-dex
-    caasp.suse.com/skuba-replica-ha: "true"
 spec:
   replicas: 3
-  revisionHistoryLimit: 0
   selector:
     matchLabels:
       app: oidc-dex
