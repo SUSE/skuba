54766464f495cc7b84344fdc9a88dca207cf40b2
Jean-Philippe Evrard <jevrard@suse.com>
2020-04-23 14:02:19 +0200

    Fix version of skuba-update
    skuba-update --version will return 0.0.0, because it's packaging
    loads a file which doesn't exist, and we forced setuptools to have
    an empty version.
    
    This is wrong because it will not allow us to version this code,
    and is silently failing.
    
    Instead we should provide a version. We could take it from the
    git code, but I opted to have a static file, VERSION, like it used
    to be. If we want to align the version of skuba-update with skuba
    tags, we can probably bump this with some kind of git tooling.
    I find the static file better for packaging, because IBS/OBS
    doesn't necessary have the git repo information for packaging the
    python code. This is simpler, and removes all code which was
    hiding the VERSION file error.

--- skuba-update/setup.py
+++ skuba-update/setup.py
@@ -14,47 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from setuptools import setup
 
 
-def version():
-    """Return the version. Pass when VERSION file not found"""
-    pwd = os.path.dirname(os.path.abspath(__file__))
-    try:
-        with open(os.path.join(pwd, '..', 'VERSION')) as version_file:
-            return version_file.read().strip()
-    except FileNotFoundError:
-        return ''
-
-
 setup(
-    name="skuba-update",
-    version=version(),
-    author="SUSE Containers Team",
-    author_email="containers@suse.com",
-    description="Utility to automatically refresh and update a SUSE CaaSP cluster nodes",
-    long_description="Wraps zypper to refresh repositories and apply non interactive patches, including patches requiring reboot.",
-    license="Apache License 2.0",
-    keywords="CaaSP",
-    url="https://github.com/SUSE/skuba-update",
-    packages=['skuba_update'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache License 2.0',
-        'Operating System :: POSIX :: Linux',
-    ], data_files=[
+    data_files=[
         (
-            'lib/systemd/system', [
-                'skuba_update/skuba-update.timer',
-                'skuba_update/skuba-update.service'
-            ]
+            "lib/systemd/system",
+            ["skuba_update/skuba-update.timer", "skuba_update/skuba-update.service"],
         ),
-        ('share/fillup-templates', ['skuba_update/sysconfig.skuba-update'])
-    ], entry_points={
-        'console_scripts': [
-            'skuba-update = skuba_update.skuba_update:main'
-        ]
-    }
+        ("share/fillup-templates", ["skuba_update/sysconfig.skuba-update"]),
+    ]
 )
