ae876c9a786952ea6c7d08a843447e3d5717ef65
Michal Jura <mjura@suse.com>
2020-05-05 20:19:01 +0200

    Update CRI-O crio.conf.d for v1.18
    For CRI-O v1.18 we are migrating configuration from
    /etc/sysconfig/crio to /etc/crio/crio.conf.d/01-caasp.conf.
    User custom configuration can be also uploaded from from
    conf.d source directory.

--- pkg/skuba/actions/cluster/init/manifests.go
+++ pkg/skuba/actions/cluster/init/manifests.go
@@ -18,6 +18,8 @@
 package cluster
 
 const (
+	CriConfFolderReadme = `This directory contains CRI-O configuration files that are uploaded to CaaSP nodes. All the files (except this README) will be uploaded to the /etc/crio/conf.d/ folder. If it is required user custom files name as 99-custom.conf
+	`
 	criDockerDefaultsConf = `## Path           : System/Management
 ## Description    : Extra cli switches for crio daemon
 ## Type           : string
@@ -25,7 +27,9 @@ const (
 ## ServiceRestart : crio
 #
 CRIO_OPTIONS=--pause-image={{.PauseImage}}{{if not .StrictCapDefaults}} --default-capabilities="CHOWN,DAC_OVERRIDE,FSETID,FOWNER,NET_RAW,SETGID,SETUID,SETPCAP,NET_BIND_SERVICE,SYS_CHROOT,KILL,MKNOD,AUDIT_WRITE,SETFCAP"{{end}}`
-	criDefaultsConf = `{{if not .StrictCapDefaults}}[crio.runtime]
+	criDefaultsConf = `# PLEASE DON'T EDIT THIS FILE!
+# This file is managed by CaaSP skuba.
+{{if not .StrictCapDefaults}}[crio.runtime]
 
 default_capabilities = [
 	"CHOWN",
