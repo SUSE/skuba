61c858d317837d34df940f5305b1b5d34b7ae5a3
Pablo Chacin <pchacin@suse.com>
2020-05-06 16:04:33 +0200

    Revert "Add provision/deploy stages to e2e tests"
    This reverts commit afd4b743fdf6ea61e6bebda8e9400e81618f9471.

--- ci/infra/testrunner/tests/test_upgrade_plan_all_fine.py
+++ ci/infra/testrunner/tests/test_upgrade_plan_all_fine.py
@@ -1,11 +1,15 @@
 import pytest
 
+from tests.utils import setup_kubernetes_version
+
+
 @pytest.mark.disruptive
 def test_upgrade_plan_all_fine(provision, skuba, kubectl, platform):
     """
     Starting from a up-to-date cluster, check what cluster/node plan report.
     """
 
+    setup_kubernetes_version(skuba, kubectl)
     out = skuba.cluster_upgrade_plan()
 
     assert out.find(
