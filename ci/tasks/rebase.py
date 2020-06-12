8a58bf9f0d43d49a7413cfe12231e4c92ee5469d
Pablo Chacin <pchacin@suse.com>
2020-06-12 15:48:52 +0200

    Support multiple target branches in pr rebase
    Rebase logic only considers pr with master as target branch.
    Adapt logic to handle multiple target branches
    
    Signed-off-by: Pablo Chacin <pchacin@suse.com>

--- ci/tasks/rebase.py
+++ ci/tasks/rebase.py
@@ -6,25 +6,26 @@ import sys
 
 change_author = os.getenv('GIT_COMMITTER_NAME', 'CaaSP Jenkins')
 author_email  = os.getenv('GIT_COMMITTER_EMAIL', 'containers-bugowner@suse.de')
-branch_name   = os.getenv("BRANCH_NAME","master")
+change_id     = os.getenv("CHANGE_ID")
+target        = os.getenv("CHANGE_TARGET")
 workspace     = os.getenv("WORKSPACE","")
 
-if branch_name.lower() == "master":
-    print("Rebase not required for master.")
+if not change_id:
+    print("Not a PR. Rebase not required.")
     sys.exit(0)
 
 try:
-    cmd = 'git -c "user.name={}" -c "user.email={}" \
-                   rebase origin/master'.format(change_author, author_email)
+    cmd = (f'git -c "user.name={change_author}" -c "user.email={author_email}"'
+           f' rebase origin/{target}')
     cwd = workspace+"/skuba"
     subprocess.check_call(cmd, cwd=cwd, shell=True)
 except subprocess.CalledProcessError as ex:
     print(ex)
-    print(Format.alert("Rebase failed, manual rebase is required."))
+    print("Rebase failed, manual rebase is required.")
     subprocess.check_call("git rebase --abort", cwd=cwd)
     sys.exit(1)
 except Exception as ex:
     print(ex)
-    print(Format.alert("Unknown error exiting."))
+    print("Unknown error exiting.")
     sys.exit(2)
 
