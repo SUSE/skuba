5e922ac274742fed4ad5004699a736bf1b8427a9
Pablo Chacin <pchacin@suse.com>
2020-06-18 13:11:59 +0200

    Replace COMMIT_ID for PR HEAD when setting status
    If the PR is automatically merged into the target branch
    then the COMMIT_ID doesn't reference the actual PR HEAD but
    the new commit after the merge, causing an error when updating
    the status of the PR in github.
    
    Signed-off-by: Pablo Chacin <pchacin@suse.com>
    (cherry picked from commit 4be9173ba61ec2b4806a5ddd33683ac34802f30a)

--- ci/jenkins/pipelines/prs/helpers/pr_manager/pr_status.py
+++ ci/jenkins/pipelines/prs/helpers/pr_manager/pr_status.py
@@ -1,23 +1,25 @@
 class PrStatus:
-    def __init__(self, build_url, repo):
-        self.build_url = build_url
+    def __init__(self, repo, change_id, build_url):
         self.repo = repo
+        self.change_id = change_id
+        self.build_url = build_url
 
-    def _create_pr_status(self, commit_sha, context, description, state):
-        commit = self.repo.get_commit(sha=commit_sha)
+    def _create_pr_status(self, context, description, state):
+        pr = self.repo.get_pull(self.change_id)
+        commit = self.repo.get_commit(sha=pr.head.sha)
         commit.create_status(state=state,
                              target_url=f'{self.build_url}display/redirect',
                              description=description,
                              context=context)
 
-    def update_pr_status(self, commit_sha, context, state):
+    def update_pr_status(self, context, state):
         if state == 'error':
-            self._create_pr_status(commit_sha, context, 'error', state)
+            self._create_pr_status(context, 'error', state)
         elif state == 'failure':
-            self._create_pr_status(commit_sha, context, 'failed', state)
+            self._create_pr_status(context, 'failed', state)
         elif state == 'pending':
-            self._create_pr_status(commit_sha, context, 'in-progress', state)
+            self._create_pr_status(context, 'in-progress', state)
         elif state == 'success':
-            self._create_pr_status(commit_sha, context, 'success', state)
+            self._create_pr_status(context, 'success', state)
         else:
             raise Exception('Unknown state')
