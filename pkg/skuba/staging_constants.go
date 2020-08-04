e82dd9c8ccc560dabace8cc58c7308774db2ed73
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2020-08-04 10:57:16 +0200

    fix staging constants
    Signed-off-by: Jordi Massaguer Pla <jmassaguerpla@suse.de>

--- pkg/skuba/staging_constants.go
+++ pkg/skuba/staging_constants.go
@@ -21,6 +21,5 @@ package skuba
 
 const (
 	BuildType         = "staging"
-	imageRepositoryV4 = "registry.suse.de/suse/sle-15-sp1/update/products/casp40/containers/caasp/v4"
-	imageRepository   = "registry.suse.de/suse/sle-15-sp2/update/products/caasp/4.5/containers/caasp/v4.5"
+	ImageRepository   = "registry.suse.de/suse/sle-15-sp2/update/products/caasp/4.5/containers/caasp/v4.5"
 )
