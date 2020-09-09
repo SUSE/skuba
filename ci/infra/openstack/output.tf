0f0a3383de7fd61a893f3e82e2bdfe906118238d
Itxaka <igarcia@suse.com>
2020-09-09 11:32:29 +0200

    [TF] Handle resource name errors
    There seems to be an issue when handling the output in openstack
    in which the resource name is not available which results into
    the output file failing to write due to zipmap operating into an
    empty tuple
    
    This patch fixes this by generating a list that match the expected
    legth no matter what.
    
    Fixes #1171653
    Fixes https://github.com/SUSE/avant-garde/issues/1469

--- ci/infra/openstack/output.tf
+++ ci/infra/openstack/output.tf
@@ -1,9 +1,19 @@
 output "ip_masters" {
-  value = "${zipmap(openstack_compute_instance_v2.master.*.name, openstack_networking_floatingip_v2.master_ext.*.address)}"
+  value = zipmap(
+    concat(
+      openstack_compute_instance_v2.master.*.name, [ for i in range(length(openstack_compute_instance_v2.master), var.masters) : format("master-%d (not provisioned)", i ) ]),
+    concat(
+      openstack_networking_floatingip_v2.master_ext.*.address,  [ for _ in range(length(openstack_networking_floatingip_v2.master_ext.*.address), var.masters) : "" ])
+    )
 }
 
 output "ip_workers" {
-  value = "${zipmap(openstack_compute_instance_v2.worker.*.name, openstack_networking_floatingip_v2.worker_ext.*.address)}"
+  value = zipmap(
+    concat(
+      openstack_compute_instance_v2.worker.*.name, [ for i in range(length(openstack_compute_instance_v2.master), var.workers) : format("worker-%d (not provisioned)", i ) ]),
+    concat(
+      openstack_networking_floatingip_v2.worker_ext.*.address,  [ for _ in range(length(openstack_networking_floatingip_v2.worker_ext.*.address), var.workers) : "" ])
+  )
 }
 
 output "ip_internal_load_balancer" {
@@ -11,5 +21,9 @@ output "ip_internal_load_balancer" {
 }
 
 output "ip_load_balancer" {
-  value = "${zipmap(list(openstack_lb_loadbalancer_v2.lb.name), list(openstack_networking_floatingip_v2.lb_ext.address))}"
+  value = zipmap(
+    length(openstack_lb_loadbalancer_v2.lb.*) == 1 ? list(openstack_lb_loadbalancer_v2.lb.name) : list("${var.stack_name}-lb"),
+    length(openstack_networking_floatingip_v2.lb_ext.*) == 1 ? list(openstack_networking_floatingip_v2.lb_ext.address) :
+      list("Could not obtain ip address, please retry terraform refresh to update the output")
+  )
 }
