7a0c136aac854cd4104124dd0fd48a8b95132f40
Clark Hsu <cclhsu@yahoo.com>
2020-11-17 12:02:37 +0800

    Update container-openrc.sh

--- ci/infra/azure/container-openrc.sh
+++ ci/infra/azure/container-openrc.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# - [How to: Use the portal to create an Azure AD application and service principal that can access resources](https://docs.microsoft.com/en-US/azure/active-directory/develop/howto-create-service-principal-portal)
-# - [Azure Provider: Authenticating using a Service Principal with a Client Secret](https://www.terraform.io/docs/providers/azurerm/guides/service_principal_client_secret.html)
-# - [Creating a Service Principal in the Azure Portal](https://www.terraform.io/docs/providers/azurerm/guides/service_principal_client_secret.html#creating-a-service-principal-in-the-azure-portal)
 
 # export ARM_CLIENT_ID=00000000-0000-0000-0000-000000000000
 # export ARM_CLIENT_SECRET=00000000-0000-0000-0000-000000000000
