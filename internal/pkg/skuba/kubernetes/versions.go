b181199a2051af98da93921c4dca157a0048ee8a
David Cassany <dcassany@suse.com>
2023-10-20 10:54:13 +0200

    Bump cilium addon version
    Signed-off-by: David Cassany <dcassany@suse.com>

--- internal/pkg/skuba/kubernetes/versions.go
+++ internal/pkg/skuba/kubernetes/versions.go
@@ -102,7 +102,7 @@ var (
 				Tooling:           &ContainerImageTag{Name: "skuba-tooling", Tag: "0.1.0"},
 			},
 			AddonsVersion: AddonsVersion{
-				Cilium:        &AddonVersion{"1.6.6-rev6", 4},
+				Cilium:        &AddonVersion{"1.6.6-rev6", 5},
 				Kured:         &AddonVersion{"1.3.0", 5},
 				Dex:           &AddonVersion{"2.16.0-rev6", 7},
 				Gangway:       &AddonVersion{"3.1.0-rev4", 6},
